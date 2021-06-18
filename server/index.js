c077cf71a9a5c5b93a2a4b2e61162466aae2805f
Ian Fragoso <you@example.com>
2021-06-17 20:49:58 -0700

    styles by diva from GET request

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 // server/indes.js
 
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(path.resolve(_dirname, '../Styles by Diva app/client/build')));
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from the server!"});
+    res.json({ message: "Styles By Diva"});
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
